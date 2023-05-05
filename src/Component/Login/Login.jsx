46fbe8e7a854062dcf048dd1b01bf88417b75d82
H3T7 <het.s@arcmentor.com>
2023-05-05 18:14:17 +0530

    changes

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import "../Login/Login.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className="container loginBox">
+        <div className="d-flex justify-content-end">
+          <AiOutlineCloseCircle className="closeIcon" />
+        </div>
+        <h2 className="text-center">BoB Event Ticketing</h2>
+        <div className="signInBox">
+          <h3>Sign In</h3>
+          <div className="loginForm">
+            <h5>Email</h5>
+            <input type="text" name="" id="" />
+            <div className="d-flex justify-content-between">
+              <h5>Password</h5>
+              <h5>Forgot Password?</h5>
+            </div>
+            <input type="password" name="" id="" />
+
+            <div className="d-flex justify-content-center">
+              <button className="signUpBtn">Sign Up</button>
+            </div>
+              <h5 className="text-center">Don't have account? <b> Sign Up</b></h5>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import "../Register/Register.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <>
+      <div className="container loginBox">
+        <div className="d-flex justify-content-end">
+          <AiOutlineCloseCircle className="closeIcon" />
+        </div>
+        <h2 className="text-center">BoB Event Ticketing</h2>
+        <div className="signUpBox">
+          <h3>Sign Up</h3>
+          <div className="loginForm">
+            <div className="row">
+              <div className="col-md-6">
+                <h5>First Name</h5>
+                <input type="text" />
+                <h5>Email</h5>
+                <input type="email" />
+                <h5>Password</h5>
+                <input type="password" />
+              </div>
+              <div className="col-md-6">
+                <h5>Last Name</h5>
+                <input type="text" />
+                <h5>Mobile No.</h5>
+                <input type="text" />
+                <h5>Confirm Password</h5>
+                <input type="password" />
+              </div>
+            </div>
+            <div className="d-flex justify-content-center">
+              <button className="signUpBtn">Sign In</button>
+            </div>
+              <h5 className="text-center">Already have an account? <b> Sign In</b></h5>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
