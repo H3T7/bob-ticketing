895c87bed4ca288254450a9f6b74736d5aad2741
H3T7 <het.s@arcmentor.com>
2023-05-05 16:40:15 +0530

    component create

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './Component/HomePage/HomePage';
+import Login from './Component/Login/Login';
+import Register from './Component/Register/Register';
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+
         </Routes>
       </BrowserRouter>
     </>
